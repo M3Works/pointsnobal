06ac6381db3726790c65c66ec87fc7242d1afefd
Micah Sandusky <micc5725@gmail.com>
2025-01-30 11:37:31 -0700

    rename folders, mac compatible install

--- setup.py
+++ setup.py
@@ -33,55 +33,60 @@ if sys.platform == 'darwin':
     from distutils import sysconfig
     vars = sysconfig.get_config_vars()
     vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
+    extra_cc_args = [
+        '-Xpreprocessor', '-fopenmp', '-lomp', '-O3', '-L./pointsnobal'
+    ]
+else:
+    extra_cc_args = ['-fopenmp', '-O3', '-L./pointsnobal']
 
 # ------------------------------------------------------------------------------
 # Compiling the C code for the Snobal library
 
-loc = 'pysnobal/c_snobal/libsnobal'
+loc = 'pointsnobal/c_snobal/libsnobal'
 cwd = os.getcwd()
 sources = glob.glob(os.path.join(loc, '*.c'))
 
-loc = 'pysnobal/c_snobal'
-extra_cc_args = ['-fopenmp', '-O3', '-L./pysnobal']
+loc = 'pointsnobal/c_snobal'
+
 sources += [os.path.join(loc, val) for val in ["snobal.pyx"]]
 extensions = [
     Extension(
-        "pysnobal.c_snobal.snobal",
+        "pointsnobal.c_snobal.snobal",
         sources,
         # libraries=["snobal"],
         include_dirs=[
             numpy.get_include(),
-            'pysnobal/c_snobal',
-            'pysnobal/c_snobal/h'
+            'pointsnobal/c_snobal',
+            'pointsnobal/c_snobal/h'
         ],
-        # runtime_library_dirs=['{}'.format(os.path.join(cwd,'pysnobal'))],
         extra_compile_args=extra_cc_args,
         extra_link_args=extra_cc_args,
     )
 ]
 
 setup(
-    name='pysnobal',
+    name='pointsnobal',
     version='0.2.0',
     description="Python wrapper of the Snobal mass and "
                 "energy balance snow model",
     long_description=readme + '\n\n' + history,
-    author="Scott Havens",
-    author_email='scott.havens@ars.usda.gov',
-    url='https://github.com/USDA-ARS-NWRC/pysnobal',
+    author="M3 Works",
+    author_email='info@m3works.io',
+    url='https://github.com/m3works/pointsnobal',
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=requirements,
-    license="CC0 1.0",
+    license="BSD license",
     zip_safe=False,
-    keywords='pysnobal',
+    keywords='pointsnobal',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: CC0 1.0',
+        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.11'
     ],
     test_suite='tests',
     tests_require=test_requirements,
